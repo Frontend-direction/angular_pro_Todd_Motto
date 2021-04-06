d45e6dbf54f0d9a6c604a3a60ca51871dbbacd00
vovapankiw <vovapankiw@gmail.com>
2021-04-06 10:37:45 +0300

    added own ngFor directives

--- 23_structural_directive/src/app/app.module.ts
+++ 23_structural_directive/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyForDirective } from './my-for/my-for.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyForDirective
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
