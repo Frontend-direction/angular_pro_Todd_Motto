a66006f4018994c7bd7f667535db0e93a994d7f9
vovapankiw <vovapankiw@gmail.com>
2021-04-06 09:37:52 +0300

    added directives with HostListener, HostBinding

--- 22_attribute_directive/src/app/app.component.ts
+++ 22_attribute_directive/src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div>
+      <label>
+        Credit Card Number
+        <input 
+          name="credit-card" 
+          type="text"
+          placeholder="Enter your 16-digit card number"
+          credit-card>
+      </label>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'directive';
 }
--- 22_attribute_directive/src/app/app.module.ts
+++ 22_attribute_directive/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardDirective
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
