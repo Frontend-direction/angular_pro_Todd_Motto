8bec2f2edf20fddd372d057b83059a64cb8aac94
vovapankiw <vovapankiw@gmail.com>
2021-04-10 09:36:23 +0300

    initial sets

---
+++ 47_routing/src/app/mail/containers/mail-folder/mail-folder.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MailFolderComponent } from './mail-folder.component';
+
+describe('MailFolderComponent', () => {
+  let component: MailFolderComponent;
+  let fixture: ComponentFixture<MailFolderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MailFolderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MailFolderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
