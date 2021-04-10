23dd405d7b0f6e954ccf0019b44ec4d2b9982e51
vovapankiw <vovapankiw@gmail.com>
2021-04-10 16:31:48 +0300

    added lazy loading

--- 47_routing/src/app/app-routing.module.ts
+++ 47_routing/src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '**', redirectTo: 'folder/inbox' }
+  {
+    path: 'dashboard', loadChildren:  () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  { 
+    path: '**', 
+    redirectTo: 'mail/folder/inbox' 
+  }
 ];
 
 @NgModule({
