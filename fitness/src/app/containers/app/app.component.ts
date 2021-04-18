57187ee6bac827547d20743605381786c215439c
vovapankiw <vovapankiw@gmail.com>
2021-04-18 18:18:55 +0300

    added user observable ot track authetification process

--- fitness/src/app/containers/app/app.component.ts
+++ fitness/src/app/containers/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Observable, Subscription } from 'rxjs';
+
+import { Store } from 'store';
+
+import { AuthService, User } from '../../../auth/shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  user$: Observable<User>;
+  subscription: Subscription;
   title = 'fitness';
+
+  constructor(
+    private store: Store,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.authService.auth$.subscribe();
+    this.user$ = this.store.select<User>('user');
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- fitness/src/auth/shared/services/auth/auth.service.ts
+++ fitness/src/auth/shared/services/auth/auth.service.ts
@@ -1,14 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { Store } from 'store';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
+import { tap } from 'rxjs/operators';
+
+export interface User {
+  email: string,
+  uid: string,
+  authenticated: boolean
+}
 
 @Injectable()
 export class AuthService {
 
   constructor(
-    private af: AngularFireAuth
+    private af: AngularFireAuth,
+    private store: Store,
   ) {}
+
+  auth$ = this.af.authState
+  .pipe(
+    tap(next => {
+      if (!next) {
+        this.store.set('user', null);
+        return;
+      }
+      const user: User = {
+        email: next.email,
+        uid: next.uid,
+        authenticated: true
+      };
+      this.store.set('user', user);
+    })
+  )
 
   createUser(email: string, password: string) {
     return this.af.createUserWithEmailAndPassword(email, password);
