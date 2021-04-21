2bba81e325954afb5a521fea9550c34cfe9a96e2
vovapankiw <vovapankiw@gmail.com>
2021-04-21 20:05:07 +0300

    added meal component

--- fitness/src/health/meals/meals.module.ts
+++ fitness/src/health/meals/meals.module.ts
@@ -7,9 +7,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { MealsComponent } from './containers/meals/meals.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { MealComponent } from './containers/meal/meal.component';
 
 export const ROUTES: Routes = [
-  { path: '', component: MealsComponent }
+  { path: '', component: MealsComponent },
+  { path: 'new', component: MealComponent }
 ];
 
 @NgModule({
@@ -20,7 +22,8 @@ export const ROUTES: Routes = [
     SharedModule,
   ],
   declarations: [
-    MealsComponent
+    MealsComponent,
+    MealComponent
   ]
 })
 export class MealsModule {}
